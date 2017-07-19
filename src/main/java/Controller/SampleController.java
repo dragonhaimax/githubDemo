3e6625ca6593a61e4fbea51ac824fe6df60b51e7
dragonhaimax <375964808@qq.com>
2017-07-19 19:20:37 +0800

    Spring Boot Hello world

--- src/main/java/Controller/SampleController.java
+++ src/main/java/Controller/SampleController.java
@@ -1,9 +1,27 @@
 package Controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by max on 18/07/2017.
  */
 
+@Controller
+@EnableAutoConfiguration
 public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class);
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by max on 18/07/2017.
  */
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
